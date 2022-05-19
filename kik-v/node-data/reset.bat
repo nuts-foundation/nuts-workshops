423cb67f654d71ea94b9af74ccac2695a72ff06c
Rein Krul <info@reinkrul.nl>
2022-05-19 12:19:53 +0200

    Reset scripts

---
+++ kik-v/node-data/reset.bat
@@ -0,0 +1,11 @@
+del one\admin\customers.json
+del one\admin\registry-admin.db
+del /S one\node\*
+
+del two\admin\customers.json
+del two\admin\registry-admin.db
+del /S two\node\*
+
+del three\admin\customers.json
+del three\admin\registry-admin.db
+del /S three\node\*
